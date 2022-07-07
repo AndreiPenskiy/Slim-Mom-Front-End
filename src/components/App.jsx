e074b8a45cbf18e5d60d711c0936352edec2e6ca
Andrei <andreipenskiy@gmail.com>
2022-07-07 18:34:58 +0300

    upd

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <li>test li</li>
+      <a href="/app"> test A</a>
+    </div>
+  );
+};
