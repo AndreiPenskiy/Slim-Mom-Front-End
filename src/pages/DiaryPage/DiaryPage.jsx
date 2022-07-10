a666e8ad78ee78c9b487624211a6e468355081c5
Mykola-Veres <veresnickolay@gmail.com>
2022-07-10 22:09:02 +0300

    add

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -2,14 +2,8 @@ import { DiaryPageConteiner } from './DiaryPage.styled';
 import DiaryDateСalendar from 'components/DiaryDateСalendar';
 import DiaryAddProductForm from 'components/DiaryAddProductForm';
 import DiaryProductsList from 'components/DiaryProductsList';
-import { useState } from 'react';
 
 export default function DiaryPage() {
-  // const [filter, setFilter] = useState('');
-  // const handlerFilterName = e => {
-  //   setFilter(e.target.value);
-  // };
-
   return (
     <DiaryPageConteiner>
       <DiaryDateСalendar />
