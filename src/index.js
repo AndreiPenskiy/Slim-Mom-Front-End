a964f71500b18b0e62a491f043091eb06aacf379
Julia Tushchenko <84866878+Julia-Tuschenko@users.noreply.github.com>
2022-07-10 18:26:23 +0300

    login and registration

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { store, persistor } from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 //import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter basename="Slim-Mom-Front-End">
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="Slim-Mom-Front-End">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,7 +1,103 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { register } from '../redux/auth/auth-options';
+import '../style/FormLoginRegistration.css';
 
 const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+  const dispath = useDispatch();
+
+  const schema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, 'The name is not entered correctly')
+      .max(20)
+      .required('Required'),
+    email: Yup.string()
+      .email('The email is not entered correctly')
+      .max(40)
+      .required('Required'),
+    password: Yup.string()
+      .min(8, `Password should be longer than 8 characters`)
+      .max(20)
+      .required('Required'),
+  });
+
+  const handleSubmit = ({
+    name,
+    email,
+    password,
+    resetForm,
+    setSubmitting,
+  }) => {
+    dispath(register({ name, email, password }));
+    setTimeout(() => {
+      resetForm();
+      setSubmitting(false);
+    }, 100);
+  };
+
+  return (
+    <>
+    {/* <section className="container"> */}
+      <Formik
+        initialValues={{ name: '', email: '', password: '' }}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        <Form className="formContainer">
+        <div className="form">
+          <h1 className="formTitle">Register</h1>
+          <div>
+            <label htmlFor="name" className="formLabel">
+              Name *
+            </label>
+            <Field id="name" name="name" type="text" className="formInput" />
+            <ErrorMessage
+              name="name"
+              render={() => (
+                <p className='alert'>The name must be at least 3 characters and more than 20</p>
+              )}
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="formLabel">
+              Email *
+            </label>
+            <Field id="email" name="email" type="email" className="formInput" />
+            <ErrorMessage
+              name="email"
+              render={() => <p className='alert'>Email should not exceed 40 characters</p>}
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="formLabel">
+              Password *
+            </label>
+            <Field
+              id="password"
+              name="password"
+              type="password"
+              className="formInput"
+            />
+            <ErrorMessage
+              name="password"
+              render={() => <p className='alert'>Password should be longer than 8 characters</p>}
+            />
+          </div>
+          </div>
+          {/* <div className="FormButtonContainer"> */}
+            <a href="http://localhost:3000/Slim-Mom-Front-End/login" className="FormButtonActive"  role="button">
+            Login                     
+            </a>
+            <button  type="submit" className="FormButton">
+              Register
+            </button>
+          {/* </div> */}
+        </Form>
+      </Formik>
+    {/* </section> */}
+    </>
+  );
 };
 
 export default RegistrationPage;
