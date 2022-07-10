e02c6018093ca80ebc5e427260cdf0e673ff14ed
Andrei <80459357+AndreiPenskiy@users.noreply.github.com>
2022-07-10 14:11:28 +0300

    Revert "login and registration"

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-import { store, persistor } from './redux/store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 //import './index.css';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="Slim-Mom-Front-End">
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>,
+  <BrowserRouter basename="Slim-Mom-Front-End">
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
