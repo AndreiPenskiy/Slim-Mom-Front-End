cedd3cbaaafb7506e05506c694ec379ea0b307a3
AndreiPenskiy <80459357+AndreiPenskiy@users.noreply.github.com>
2022-07-12 12:29:18 +0000

    Deploying to gh-pages from @ AndreiPenskiy/Slim-Mom-Front-End@880df421445ea89d10c54353d79ad6e0ce9fc149 🚀

--- static/js/main-page.901173c4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[143],{727:function(e,t,a){a.r(t);a(2791);var r=a(184);t.default=function(){return(0,r.jsx)("div",{children:"MainPage"})}}}]);
-//# sourceMappingURL=main-page.901173c4.chunk.js.map
